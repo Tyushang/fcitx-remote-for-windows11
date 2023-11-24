538b23aa29eb476a0e4d972d4387794d89d9eeb3
frank-jing <frank-jing@qq.com>
2023-11-24 15:09:30 +0800

    Init commit. and works fine on windows11.

---
+++ fcitx-remote/fcitx-remote-by-hotkey.cpp
@@ -0,0 +1,68 @@
+// fcitx-remote.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <iostream>
+#pragma comment(lib, "user32.lib")
+
+using namespace std;
+
+// The first parameter is not correct, but it works. So it's fine here.
+static const HKL ENG_LAYOUT = LoadKeyboardLayout((LPCWSTR)"00000409", 2);
+
+BOOL isEngLayout()
+{
+	HWND hwnd = GetForegroundWindow(); // 获取当前活动窗口的句柄
+	DWORD tid = GetWindowThreadProcessId(hwnd, NULL);
+
+	return GetKeyboardLayout(tid) == ENG_LAYOUT;
+}
+
+
+BOOL toggleInputMethod()
+{
+    INPUT ips[4];
+    memset(ips, 0, sizeof(ips));
+    for (int i = 0; i < 4; i++) {
+        ips[i].type = INPUT_KEYBOARD;
+    }
+    // uncomment if use win+space
+    //ips[0].ki.wVk = ips[2].ki.wVk = VK_LWIN;
+    //ips[1].ki.wVk = ips[3].ki.wVk = VK_SPACE;
+    ips[0].ki.wVk = ips[2].ki.wVk = VK_MENU;
+    ips[1].ki.wVk = ips[3].ki.wVk = VK_SHIFT;
+    ips[2].ki.dwFlags = ips[3].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(4, ips, sizeof(INPUT));
+    return 0;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    if (argc > 1)
+    {
+        if (!_tcscmp(argv[1], _T("-c")))
+        {
+            if (!isEngLayout())
+            {
+                toggleInputMethod();
+            }
+        }
+        else if (!_tccmp(argv[1], _T("-o")))
+        {
+            if (isEngLayout())
+            {
+                toggleInputMethod();
+            }
+        }
+    }
+    else
+    {
+        cout << (isEngLayout() ? 1 : 2) << endl;
+        // cout << (2 - isEngLayout()) << endl;
+    }
+    return 0;
+}
+
+
+
---
+++ fcitx-remote/fcitx-remote.cpp
@@ -0,0 +1,55 @@
+﻿#include "stdafx.h"
+
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+#define EN_US 0x0409
+#define ZH_CN 0x0804
+
+/*
+*  Check if current keyboard layout is English.
+*/
+bool IsEnglish() {
+    HKL hkl = GetKeyboardLayout(
+        GetWindowThreadProcessId(GetForegroundWindow(), NULL)
+    );
+    WORD layoutId = LOWORD(hkl);
+    return layoutId == EN_US;
+}
+
+void ToggleEnglishOrNot(bool EnglishOrNot) {
+    if (EnglishOrNot) {
+        // Set KeyboardLayout to English.
+		PostMessage(GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 0, EN_US);
+    }
+    else {
+        // Set KeyboardLayout to Non English.
+		if (IsEnglish()) {
+			PostMessage(GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 
+                INPUTLANGCHANGE_FORWARD, HKL_NEXT);
+        }
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[]) {
+    if (argc > 1) {
+        // Toggle english if 1st arg is "-c";
+        if (!_tcscmp(argv[1], _T("-c"))) {
+            ToggleEnglishOrNot(true);
+        }
+        // Toggle non english if 1st arg is "-o" ;
+        else if (!_tcscmp(argv[1], _T("-o"))) {
+            ToggleEnglishOrNot(false);
+        }
+    } else {  // if no arg, print 1 for english, 2 for non english.
+        cout << (IsEnglish() ? 1 : 2) << endl;
+    }
+
+    return 0;
+}
+
+
+
+
